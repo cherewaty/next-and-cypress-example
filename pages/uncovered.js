da2b826e0f4fb2582dea8b0ac5d362255dea79e5
Jeffrey Cherewaty <jeffrey@frontside.com>
2020-11-08 13:56:11 -0600

    Add page uncovered by tests

---
+++ pages/uncovered.js
@@ -0,0 +1,9 @@
+import Layout from '../components/MyLayout.js'
+
+export default function Uncovered() {
+  return (
+    <Layout>
+      <p>This is a page uncovered by tests</p>
+    </Layout>
+  )
+}
